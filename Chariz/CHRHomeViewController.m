968f4e4a7a02bb93754f9a926e56d6787f92e01b
Adam D <kirbylover4000@gmail.com>
2015-04-06 23:39:37 +0930

    how do i even search field

--- Chariz/CHRHomeViewController.m
+++ Chariz/CHRHomeViewController.m
@@ -16,6 +16,16 @@
 	self.title = L18N(@"Home");
 	
 	[self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"/featured/" relativeToURL:[NSURL URLWithString:kCHRWebUIRootURL]]]];
+	
+	NSSearchField *searchField = [[NSSearchField alloc] initWithFrame:CGRectMake(0, 0, 150.f, 22.f)];
+	// searchField.delegate = self;
+	searchField.shadow = [[NSShadow alloc] init];
+	
+	NSSearchFieldCell *searchCell = searchField.cell;
+	searchCell.wraps = NO;
+	searchCell.scrollable = YES;
+	
+	self.navigationItem.rightBarButtonItem = [[UXBarButtonItem alloc] initWithCustomView:searchField];
 }
 
 @end
